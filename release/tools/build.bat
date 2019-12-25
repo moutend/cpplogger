a17cde7578cd44541ab1fe4bc7046a88f151f322
Yoshiyuki Koyanagi <moutend@gmail.com>
2019-12-25 12:50:31 +0900

    Chore

--- release/tools/build.bat
+++ release/tools/build.bat
@@ -1,3 +1,3 @@
 @echo off
 
-cmake -DBUILD__TOOLS=yes -DCMAKE_GENERATOR_PLATFORM=x64 -DCMAKE_TOOLCHAIN_FILE=C:/Users/koyanagi/github.com/microsoft/vcpkg/scripts/buildsystems/vcpkg.cmake ..
+cmake -DBUILD_TESTS=yes -DCMAKE_GENERATOR_PLATFORM=x64 -DCMAKE_TOOLCHAIN_FILE=C:/Users/koyanagi/github.com/microsoft/vcpkg/scripts/buildsystems/vcpkg.cmake ..
